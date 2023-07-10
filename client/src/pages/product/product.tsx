1d6684663f7dd1591b9da37276b331c870eea34e
anKhav <khavaldzhi.a@gmail.com>
2023-07-10 19:10:12 +0200

    Added common structure and common styles for Specific Product with fetched data

--- client/src/pages/product/product.tsx
+++ client/src/pages/product/product.tsx
@@ -1,9 +1,20 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {axiosBase} from "../../API/axiosApi.ts";
+import styles from  './index.module.css'
+import {EXTERNAL_SERVER_BASE_URL} from "../consts.ts";
+
+interface Product {
+    id:number,
+    name:string,
+    images:[string],
+    price:number,
+    categories:[string],
+    sizes:[string]
+}
 
 const Product = () => {
-    const [product, setProduct] = useState({})
+    const [product, setProduct] = useState<Product | null>(null)
     useEffect(() => {
         axiosBase.get(`/product/${id}`).then(res => res).then(data => setProduct(data.data))
     },[])
@@ -11,9 +22,30 @@ const Product = () => {
     const {id} = useParams()
     console.log(id)
     return (
-        <div>
-            Product {id}
-        </div>
+        <main className={styles.product}>
+            {
+                product &&
+                <>
+                    <div className={styles.img__wrapper}>
+                        <img className={styles.img} src={`${EXTERNAL_SERVER_BASE_URL}/${product.images[0]}`} alt=""/>
+                    </div>
+                    <div className={styles.product__content}>
+                        <h1 className={styles.product__title}>{product.name}</h1>
+                        <span className={styles.product__price}>{product.price} $</span>
+                        <div className={styles.product__sizes}>
+                            {
+                                product.sizes.map(size => <div className={styles.size}>{size}</div>)
+                            }
+                        </div>
+                        <div className={styles.product__categories}>
+                            {
+                                product.categories.map(category => <div className={styles.category}>{category}</div>)
+                            }
+                        </div>
+                    </div>
+                </>
+            }
+        </main>
     );
 };
 
